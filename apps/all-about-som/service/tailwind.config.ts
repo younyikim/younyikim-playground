74de75e036cf4157da8ca4163822613be46a5cb2
Younyi Kim <rlakuku1221@gmail.com>
2024-06-17 15:22:10 +0900

    feat(main): 폰트 추가
    main

--- apps/all-about-som/service/tailwind.config.ts
+++ apps/all-about-som/service/tailwind.config.ts
@@ -31,6 +31,10 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      fontFamily: {
+        gulf: ['Gulf Display', 'sans-serif'],
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
