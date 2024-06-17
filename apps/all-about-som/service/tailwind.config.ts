ee6f026b34c5ad67ef8f3d8242a2bd849582e02d
Younyi Kim <rlakuku1221@gmail.com>
2024-06-17 16:19:50 +0900

    feat(main): 폰트 사이즈, 색상 추가
    main

--- apps/all-about-som/service/tailwind.config.ts
+++ apps/all-about-som/service/tailwind.config.ts
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const baseFontSize = 10;
+
 module.exports = {
   darkMode: ['class'],
   content: [
@@ -31,9 +33,70 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      colors: {
+        blue: '#0059F5',
+        pink: '#F6AFC6',
+        yellow: '#FFF06C',
+      },
       fontFamily: {
         gulf: ['Gulf Display', 'sans-serif'],
         pretendard: ['Pretendard', 'sans-serif'],
+      },
+      fontSize: {
+        xs: [
+          `${(16 * 0.75) / baseFontSize}rem` /* 12px */,
+          {
+            lineHeight: `${(16 * 1) / baseFontSize}rem` /* 16px */,
+          },
+        ],
+        sm: [
+          `${(16 * 0.875) / baseFontSize}rem` /* 14px */,
+          {
+            lineHeight: `${(16 * 1.25) / baseFontSize}rem` /* 20px */,
+          },
+        ],
+        base: [
+          `${(16 * 1) / baseFontSize}rem` /* 16px */,
+          {
+            lineHeight: `${(16 * 1.5) / baseFontSize}rem` /* 24px */,
+          },
+        ],
+        lg: [
+          `${(16 * 1.125) / baseFontSize}rem` /* 18px */,
+          {
+            lineHeight: `${(16 * 1.75) / baseFontSize}rem` /* 28px */,
+          },
+        ],
+        xl: [
+          `${(16 * 1.25) / baseFontSize}rem` /* 20px */,
+          {
+            lineHeight: `${(16 * 1.75) / baseFontSize}rem` /* 28px */,
+          },
+        ],
+        '2xl': [
+          `${(16 * 1.5) / baseFontSize}rem` /* 24px */,
+          {
+            ineHeight: `${(16 * 2) / baseFontSize}rem` /* 32px */,
+          },
+        ],
+        '3xl': [
+          `${(16 * 1.875) / baseFontSize}rem` /* 30px */,
+          {
+            lineHeight: `${(16 * 2.25) / baseFontSize}rem` /* 36px */,
+          },
+        ],
+        '4xl': [
+          `${(16 * 2.25) / baseFontSize}rem` /* 36px */,
+          {
+            lineHeight: `${(16 * 2.5) / baseFontSize}rem` /* 40px */,
+          },
+        ],
+        '5xl': [
+          `${(16 * 3) / baseFontSize}rem` /* 48px */,
+          {
+            lineHeight: (16 * 1) / baseFontSize,
+          },
+        ],
       },
     },
   },
