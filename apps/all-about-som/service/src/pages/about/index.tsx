8b677b2083b993f3f831fea8eaed01848fa3f4b3
Younyi Kim <rlakuku1221@gmail.com>
2024-06-19 17:50:26 +0900

    feat(all-about-som): about 페이지 이미지 카드 추가
    all-about-som

--- apps/all-about-som/service/src/pages/about/index.tsx
+++ apps/all-about-som/service/src/pages/about/index.tsx
@@ -11,10 +11,14 @@ const index = forwardRef((_, ref: ForwardedRef<HTMLDivElement>) => {
   return (
     <section
       ref={ref}
-      className="bg-yellow relative flex h-screen w-full flex-col items-center"
+      className="bg-yellow relative flex h-screen w-full flex-col items-center overflow-hidden"
     >
       <Title />
       <Card />
+      <div className="flex w-full items-start justify-between px-20 pt-16">
+        <img src={Images.flower} alt="Flower icon" />
+        <img src={Images.starGreen} alt="Star icon" />
+      </div>
       {/* 하단 원형 아이콘 */}
       <div className="dots">
         <img src={Images.circle} alt="Circle image" />
