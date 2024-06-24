b75870656b63cbc9d7afe53f4f872be7b22b66eb
Younyi Kim <rlakuku1221@gmail.com>
2024-06-24 22:33:33 +0900

    feat(main): ui 라이브러리 storybook 추가
    main

---
+++ packages/ui/src/stories/Button.stories.ts
@@ -0,0 +1,24 @@
+import { Button } from '@components/button';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Button',
+  },
+};
