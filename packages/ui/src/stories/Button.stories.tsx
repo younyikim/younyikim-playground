28e50934e2f295410bc9f231acf9c4e3a171492a
Younyi Kim <rlakuku1221@gmail.com>
2024-06-24 22:54:41 +0900

    feat(main): ui 라이브러리 alert, button storybook 추가
    main

--- packages/ui/src/stories/Button.stories.tsx
+++ packages/ui/src/stories/Button.stories.tsx
@@ -1,6 +1,8 @@
-import { Button } from '@components/button';
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+
+// Component
+import { Button } from '@components/button';
 
 const meta = {
   title: 'Example/Button',
