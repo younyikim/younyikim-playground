712b0fa09fe028cf94be9ecff5d7c86307226239
Younyi Kim <rlakuku1221@gmail.com>
2024-06-24 16:38:35 +0900

    feat(all-about-som): details-information 카드 공통 컴포넌트 추가
    all-about-som

---
+++ apps/all-about-som/service/src/pages/details/components/infoCard/index.tsx
@@ -0,0 +1,34 @@
+import { InfoCardProps } from '@pages/details/components/infoCard/types';
+
+const index = (props: InfoCardProps) => {
+  const {
+    selected,
+    cardIndex,
+    className,
+    handleSelected,
+    faceContent,
+    backContent,
+  } = props;
+
+  return (
+    <div
+      className={`box-border flex flex-1 cursor-pointer items-center justify-center border border-black ${className}`}
+      onClick={() =>
+        selected === cardIndex ? handleSelected(-1) : handleSelected(cardIndex)
+      }
+    >
+      <div
+        className={`${selected === cardIndex ? 'hidden' : 'visible'} h-full w-full`}
+      >
+        {faceContent}
+      </div>
+      <div
+        className={`${selected === cardIndex ? 'visible' : 'hidden'} h-full w-full`}
+      >
+        {backContent}
+      </div>
+    </div>
+  );
+};
+
+export default index;
---
+++ apps/all-about-som/service/src/pages/details/components/infoCard/types.d.ts
@@ -0,0 +1,8 @@
+export interface InfoCardProps {
+  selected: number;
+  handleSelected: (index: number) => void;
+  cardIndex: number;
+  className?: string;
+  faceContent: ReactNode;
+  backContent: ReactNode;
+}
