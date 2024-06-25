5994b88a0d4e62e33e3456816cc136cf1c6c0bf9
Younyi Kim <rlakuku1221@gmail.com>
2024-06-25 15:34:44 +0900

    feat(main): ui 라이브러리 index.ts 업데이트
    main

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,9 @@
 export { Button, buttonVariants, type ButtonProps } from '@components/button';
+export {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+  alertVariants,
+} from '@components/alert';
+export { Label, labelVariants } from '@components/label';
+export { Switch } from '@components/switch';
