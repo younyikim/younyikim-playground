c6b6a1c39e959b504762b2d8d6896136d5191f46
Younyi Kim <rlakuku1221@gmail.com>
2024-06-25 15:27:17 +0900

    feat(main): ui 라이브러리 switch 컴포넌트 추가
    main

---
+++ packages/ui/src/stories/Switch.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import * as React from 'react';
+
+// Component
+import { Switch } from '@/components/switch';
+import { Label } from '@components/label';
+
+// Alert 컴포넌트의 Prop 타입 정의
+interface SwitchProps {
+  children: React.ReactNode;
+}
+
+const meta: Meta<React.FC<SwitchProps>> = {
+  title: 'Example/Switch',
+  component: Switch,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<React.FC<SwitchProps>>;
+
+export const Basic: Story = {
+  render: () => <Switch />,
+};
+
+export const SwitchWithIcon: Story = {
+  render: () => (
+    <Switch
+      src={
+        'https://fonts.gstatic.com/s/i/materialicons/check_circle/v15/24px.svg'
+      }
+    />
+  ),
+};
+
+export const SwitchWithTag: Story = {
+  render: () => (
+    <div className="flex items-center space-x-2">
+      <Switch />
+      <Label htmlFor="airplane-mode">Switch Tag</Label>
+    </div>
+  ),
+};
