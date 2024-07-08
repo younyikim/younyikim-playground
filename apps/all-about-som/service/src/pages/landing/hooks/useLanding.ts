7effb8d6b0483252dd19e94b68fa709f555f7b5d
Younyi Kim <rlakuku1221@gmail.com>
2024-07-08 12:54:57 +0900

    fix(all-about-som): font preload 추가
    all-about-som

--- apps/all-about-som/service/src/pages/landing/hooks/useLanding.ts
+++ apps/all-about-som/service/src/pages/landing/hooks/useLanding.ts
@@ -4,11 +4,11 @@ import { useEffect, useState } from 'react';
 import { Images } from '@utils/image';
 
 const useLanding = () => {
-  const [currentImage, setCurrentImage] = useState(Images.landing2);
+  const [currentImage, setCurrentImage] = useState(Images.landing1);
 
   useEffect(() => {
     const images = [Images.landing1, Images.landing2];
-    let currentIndex = 0;
+    let currentIndex = 1;
 
     // 3초에 한 번 이미지를 변경
     const interval = setInterval(() => {
