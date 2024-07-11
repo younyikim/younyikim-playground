5a14f1076967cf424a54770a5798a7128378dcbc
Younyi Kim <rlakuku1221@gmail.com>
2024-07-11 20:00:16 +0900

    feat(all-about-som): 관리자 로그인 페이지 컴포넌트 추가
    all-about-som

--- apps/all-about-som/service/src/pages/admin/components/signIn/index.tsx
+++ apps/all-about-som/service/src/pages/admin/components/signIn/index.tsx
@@ -1,5 +1,16 @@
+import { SignInForm } from '@younyikim/ui';
+
 const index = () => {
-  return <div>로그인</div>;
+  return (
+    <div className="scrollbar-hide h-real-screen flex w-full flex-col items-center justify-center overflow-hidden px-20">
+      <h3 className="text-3xl font-bold">관리자 로그인</h3>
+      <SignInForm
+        onSubmit={() => {
+          console.log('로그인');
+        }}
+      />
+    </div>
+  );
 };
 
 export default index;
