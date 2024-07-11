d0bae2782aee17627cc63c6241472da181ee2a6b
Younyi Kim <rlakuku1221@gmail.com>
2024-07-11 16:37:01 +0900

    feat(all-about-som): react query 추가
    all-about-som

--- apps/all-about-som/service/src/App.tsx
+++ apps/all-about-som/service/src/App.tsx
@@ -1,11 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
 import '@younyikim/ui/dist/style.css';
 
+// Apis
+import { queryClient } from '@apis/react-query';
+
 // Utils
-import router from './utils/router';
+import router from '@utils/router';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
