03285b3d413812dd48e082b4f307ad028cd74c15
Younyi Kim <rlakuku1221@gmail.com>
2024-07-11 12:14:41 +0900

    feat(all-about-som): jwt 인증 미들웨어 추가
    all-about-som

---
+++ apps/all-about-som/server/src/routes/status.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+// Middleware
+import { verifyToken } from '@/middlewares';
+
+const router = express.Router();
+
+// 현재 솜이 위치 정보
+router.get('/status', verifyToken, () => {});
+
+export default router;
