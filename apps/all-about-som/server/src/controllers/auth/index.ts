6bc715d01fbd24a5d6534edd747ea2e11c3cd3fe
Younyi Kim <rlakuku1221@gmail.com>
2024-07-12 13:39:32 +0900

    feat(all-about-som): server 유저 로그인 여부 판별 api 추가
    all-about-som

--- apps/all-about-som/server/src/controllers/auth/index.ts
+++ apps/all-about-som/server/src/controllers/auth/index.ts
@@ -2,5 +2,6 @@ import { signUp } from './signUp';
 import { signIn } from './signIn';
 import { signOut } from './signOut';
 import { token } from './token';
+import { verifySignIn } from './verifySignIn';
 
-export { signUp, signIn, signOut, token };
+export { signUp, signIn, signOut, token, verifySignIn };
