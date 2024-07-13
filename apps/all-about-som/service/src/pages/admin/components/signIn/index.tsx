8c139d41d13bfe754887242313706a5a21501bfd
Younyi Kim <rlakuku1221@gmail.com>
2024-07-13 12:22:47 +0900

    style(all-about-som): admin/sign-in 페이지 스타일 수정
    all-about-som

--- apps/all-about-som/service/src/pages/admin/components/signIn/index.tsx
+++ apps/all-about-som/service/src/pages/admin/components/signIn/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
   const { handleSubmit } = useSignIn();
 
   return (
-    <section className="scrollbar-hide flex h-svh w-full flex-col items-center justify-center overflow-hidden px-20">
+    <section className="scrollbar-hide flex h-svh w-full flex-col items-center justify-center gap-16 overflow-hidden px-20">
       <h3 className="text-3xl font-bold">관리자 로그인</h3>
       <SignInForm onSubmit={handleSubmit} />
     </section>
