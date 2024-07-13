05b7680bcfe4589df0a5bf7b4028b7f5f6fc73c0
Younyi Kim <rlakuku1221@gmail.com>
2024-07-13 11:42:12 +0900

    feat(all-about-som): 토큰 갱신 axios interceptor 추가
    all-about-som

--- apps/all-about-som/service/src/apis/axios/methods.ts
+++ apps/all-about-som/service/src/apis/axios/methods.ts
@@ -1,8 +1,55 @@
+import axios from 'axios';
+
 // Utils
 import { axiosInstance, createAxiosInstance } from '@apis/axios';
 
 // Typings
 import { ParamsType } from './typings/axios';
+import { routes } from '@utils/router';
+import { apiEndpoints } from '@apis/endpoints';
+
+// 토큰 갱신 요청 중복 방지를 위해 현재 진행 중인 요청을 저장하는 변수
+let refreshTokenPromise: Promise<unknown> | null = null;
+
+// axios 응답 interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    // 에러가 발생한 요청
+    const originalRequest = error.config;
+
+    // Unauthorized 에러
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      // 현재 진행 중인 토큰 갱신 요청이 없으면 새로운 토큰 갱신 요청 생성
+      if (!refreshTokenPromise) {
+        refreshTokenPromise = post(apiEndpoints.auth.token).then(() => {
+          refreshTokenPromise = null;
+        });
+      }
+
+      // 토큰 갱신 요청이 완료되면 원래의 요청을 재시도
+      return refreshTokenPromise
+        .then(() => {
+          // 에러가 발생했던 기존 요청을 다시 시도
+          axios(originalRequest);
+        })
+        .catch(() => {
+          // 토큰 갱신에 실패한 경우 로그인 페이지로 리다이렉트
+          const currentPath = window.location.pathname;
+
+          if (currentPath !== routes.admin.signIn) {
+            window.location.pathname = routes.admin.signIn;
+          }
+
+          return Promise.reject(error);
+        });
+    }
+  },
+);
 
 /**
  * @description Axios GET 요청 메서드
