6a0c9114131110096d20f4daac8229dfbc0f619c
Younyi Kim <rlakuku1221@gmail.com>
2024-07-23 10:56:20 +0900

    fix(main): router 분리
    main

--- apps/all-about-som/server/api/index.ts
+++ apps/all-about-som/server/api/index.ts
@@ -8,8 +8,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 // Routes
-import authRoutes from './routes/auth';
-import statusRoutes from './routes/status';
+import { router } from './routes';
 
 // dotenv 모듈을 사용해 환경변수 로드
 dotenv.config();
@@ -28,13 +27,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api', (_, res) => res.send({ message: 'Express on Vercel' }));
-//auth로 시작하는 모든 요청은 authRoutes에 정의된 라우터로 전달
-app.use('/api/auth', authRoutes);
-app.use('/api/status', statusRoutes);
-app.get('/api/test', (_, res) => {
-  res.send('Hello from server!');
-});
+router(app);
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
--- apps/all-about-som/server/api/routes/index.ts
+++ apps/all-about-som/server/api/routes/index.ts
@@ -4,15 +4,12 @@ import { Application } from 'express';
 import authRoutes from './auth';
 import statusRoutes from './status';
 
-// Middlewares
-import { verifyToken } from '../../middlewares';
-
 export const router = (app: Application) => {
   app.get('/', (_, res) => res.send({ message: 'Express on Vercel' }));
   //auth로 시작하는 모든 요청은 authRoutes에 정의된 라우터로 전달
   app.use('/api/auth', authRoutes);
   app.use('/api/status', statusRoutes);
-  app.get('/api/test', verifyToken, (_, res) => {
+  app.get('/api/test', (_, res) => {
     res.send('Hello from server!');
   });
 };
