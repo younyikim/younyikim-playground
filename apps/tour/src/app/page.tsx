f239ec0ffb49f5ea1d6d3189f0e54b6a2e800d11
younyikim <rlakuku1221@gmail.com>
2024-10-26 17:12:27 +0900

    feat(tour): tour 프로젝트 생성
    tour

---
+++ apps/tour/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="= grid min-h-screen items-center justify-items-center">
+      <div>Next.js Template Project</div>
+    </section>
+  );
+}
